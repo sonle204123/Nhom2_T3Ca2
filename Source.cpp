9b9eb69c2c4b80ec1d4de49150dee2a741e3023f
Son <lengocson.2004tq@gmail.com>
2025-11-04 11:05:31 +0700

    Viet ham tinh tong

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+
+int tong(int a, int b)
+{
+	int tong = a + b;
+	return tong;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	int a, b;
+	cout << "Nhap so a:";
+	cin >> a;
+	cout << "Nhap so b:";
+	cin >> b;
+	cout << "Tong =" << tong(a, b);
 	system("pause");
 	return 0;
 }
