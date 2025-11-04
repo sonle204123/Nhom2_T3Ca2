0b3e63e9e02a40ce95b3fc556f795e74b57d3e02
Tran Quang Vinh <tranvinh4464@gmail.com>
2025-11-04 11:16:14 +0700

    giai quyet xung dot

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,19 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	return a-b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a=10;
+	int b=5;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	cout << "Sinh viên B thực hiện" << endl;
+	cout<<"Hieu =" << hieu(a,b);
 	system("pause");
 	return 0;
 }
